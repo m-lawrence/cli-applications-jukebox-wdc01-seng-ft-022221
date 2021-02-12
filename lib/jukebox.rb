f1d0b45d3a31ec173468d0c25ba24b5f4cd103ed
Melissa Lawrence <mlawrence537@gmail.com>
2021-02-12 17:14:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -56,8 +56,9 @@ end
 
 def run(songs)
   puts "Please enter a command:"
-  user_command = gets.strip 
-  until user_command == "exit"
+ 
+  until user_command == "exit" do
+     user_command = gets.strip 
   if user_command == "list"
     list(songs)
     elsif user_command == "help"
