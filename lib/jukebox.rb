1480d9e188ac09d95c70c6e504af37c4414c1ba4
Melissa Lawrence <mlawrence537@gmail.com>
2021-02-12 17:34:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -62,8 +62,8 @@ def run(songs)
   if user_command == "exit"
     exit_jukebox
   end
-  binding.pry
   while user_command != "exit" do
+    binding.pry
      user_command = gets.strip 
     if user_command == "list"
     list(songs)
